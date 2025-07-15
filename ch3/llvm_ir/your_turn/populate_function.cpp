e58c2a9fd054e0c10ef5cdb25c83ff6897e68cca
omarahmed1111 <omar.ahmed@codeplay.com>
2025-07-15 15:06:52 +0100

    Add solution for llvm_ir ch3

--- ch3/llvm_ir/your_turn/populate_function.cpp
+++ ch3/llvm_ir/your_turn/populate_function.cpp
@@ -57,4 +57,52 @@ using namespace llvm;
 //
 // declare void @bar(i32)
 // declare i32 @baz(...)
-std::unique_ptr<Module> myBuildModule(LLVMContext &Ctxt) { return nullptr; }
+std::unique_ptr<Module> myBuildModule(LLVMContext &Ctxt) { 
+    std::unique_ptr<Module> ModulePtr = std::make_unique<Module>("MyModule", Ctxt);
+
+    FunctionType* bazFnType = FunctionType::get(Type::getInt32Ty(Ctxt), {}, false);
+    FunctionType* barFnType = FunctionType::get(Type::getVoidTy(Ctxt), {Type::getInt32Ty(Ctxt)}, false);
+    ModulePtr->getOrInsertFunction("baz", bazFnType);
+    ModulePtr->getOrInsertFunction("bar", barFnType);
+
+    FunctionType* fooFnType = FunctionType::get(Type::getVoidTy(Ctxt), {Type::getInt32Ty(Ctxt), Type::getInt32Ty(Ctxt)}, false);
+    Function *fooFn = Function::Create(fooFnType, GlobalValue::ExternalLinkage, 0, "foo", &*ModulePtr);
+
+    // Basic blocks
+    BasicBlock* bb = BasicBlock::Create(Ctxt, "bb", fooFn);
+    BasicBlock* bb9 = BasicBlock::Create(Ctxt, "bb9", fooFn);
+    BasicBlock* bb12 = BasicBlock::Create(Ctxt, "bb12", fooFn);
+
+    // IR Builders
+    IRBuilder bbB(bb);
+    IRBuilder bb9B(bb9);
+    IRBuilder bb12B(bb12);
+    
+    // bb instructions
+    auto *alloca1 = bbB.CreateAlloca(Type::getInt32Ty(Ctxt));
+    auto *alloca2 = bbB.CreateAlloca(Type::getInt32Ty(Ctxt));
+    auto *alloca3 = bbB.CreateAlloca(Type::getInt32Ty(Ctxt));
+    bbB.CreateStore(fooFn->getArg(0), alloca1);
+    bbB.CreateStore(fooFn->getArg(1), alloca2);
+    auto* load1 = bbB.CreateLoad(Type::getInt32Ty(Ctxt), alloca1);
+    auto* load2 = bbB.CreateLoad(Type::getInt32Ty(Ctxt), alloca2);
+    auto* add = bbB.CreateAdd(load1, load2);
+    bbB.CreateStore(add, alloca3);
+    auto* load3 = bbB.CreateLoad(Type::getInt32Ty(Ctxt), alloca3);
+    auto* icmp = bbB.CreateICmp(CmpInst::ICMP_EQ, load3, ConstantInt::get(Type::getInt32Ty(Ctxt), 255));
+    bbB.CreateCondBr(icmp, bb9, bb12);
+
+    // bb9 instructions
+    auto* load4 = bb9B.CreateLoad(Type::getInt32Ty(Ctxt), alloca3);
+    bb9B.CreateCall(barFnType, ModulePtr->getNamedValue("bar"), {load4});
+    auto* call = bb9B.CreateCall(bazFnType, ModulePtr->getNamedValue("baz"), {});
+    bb9B.CreateStore(call, alloca3);
+    bb9B.CreateBr(bb12);
+
+    // bb12 instructions
+    auto* load5 = bb12B.CreateLoad(Type::getInt32Ty(Ctxt), alloca3);
+    bb12B.CreateCall(barFnType, ModulePtr->getNamedValue("bar"), {load5});
+    ReturnInst::Create(Ctxt, bb12);
+
+    return ModulePtr;
+}
