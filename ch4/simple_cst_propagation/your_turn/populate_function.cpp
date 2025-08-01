f0242862257d237e238c7f3507399fd0ab12237d
omarahmed1111 <omar.ahmed@codeplay.com>
2025-08-02 00:08:52 +0100

    Retrive the const from shl instruction in ch4

--- ch4/simple_cst_propagation/your_turn/populate_function.cpp
+++ ch4/simple_cst_propagation/your_turn/populate_function.cpp
@@ -11,11 +11,26 @@
 #include "llvm/IR/InstrTypes.h" // For ICMP_EQ.
 
 using namespace llvm;
-
 // Takes \p Foo and apply a simple constant propagation optimization.
 // \returns true if \p Foo was modified (i.e., something had been constant
 // propagated), false otherwise.
 bool myConstantPropagation(Function &Foo) {
-  // TODO: populate this function.
+  for (auto& BB : Foo) {
+    for (auto& I : BB) {
+      if (auto* shlI = dyn_cast<ShlOperator>(&I)) {
+        Value* OperandOne = shlI->getOperand(0);
+        Value* OperandTwo = shlI->getOperand(1);
+        auto* ConstOne = dyn_cast<ConstantInt>(OperandOne);
+        auto* ConstTwo = dyn_cast<ConstantInt>(OperandTwo);
+
+        if (ConstOne && ConstTwo) {
+          APInt NewConst = ConstOne->getValue() << ConstTwo->getValue();
+          outs() << "\n\n\n\n\n\n\n\n" << NewConst << "\n\n\n\n\n\n";
+        }
+
+        // outs() << "\n\n\n\n\n\n\n\n" << ConstOne->getValue() << "\n\n\n\n\n\n";
+      }
+    }
+  }
   return false;
 }
