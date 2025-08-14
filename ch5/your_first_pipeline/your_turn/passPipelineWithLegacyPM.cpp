c9f5247ba05fa085673fd95950a08df57ee71e89
omarahmed1111 <omar.ahmed@codeplay.com>
2025-08-14 17:22:15 +0100

    [ch5] Add solution to ch5 secnd excersice

--- ch5/your_first_pipeline/your_turn/passPipelineWithLegacyPM.cpp
+++ ch5/your_first_pipeline/your_turn/passPipelineWithLegacyPM.cpp
@@ -1,5 +1,16 @@
 #include "llvm/IR/LegacyPassManager.h" // For legacy::PassManager.
+#include "llvm/Transforms/Utils.h"
+#include "llvm/Transforms/InstCombine/InstCombine.h"
+#include "llvm/Transforms/IPO/AlwaysInliner.h"
 
 using namespace llvm;
 
-void runYourTurnPassPipelineForLegacyPM(Module &MyModule) {}
+void runYourTurnPassPipelineForLegacyPM(Module &MyModule) {
+    legacy::PassManager LegacyPM;
+
+    LegacyPM.add(createPromoteMemoryToRegisterPass());
+    LegacyPM.add(createInstructionCombiningPass());
+    LegacyPM.add(createAlwaysInlinerLegacyPass());
+
+    LegacyPM.run(MyModule);
+}
--- ch5/your_first_pipeline/your_turn/passPipelineWithNewPM.cpp
+++ ch5/your_first_pipeline/your_turn/passPipelineWithNewPM.cpp
@@ -1,6 +1,44 @@
 #include "llvm/IR/Module.h"
 #include "llvm/IR/PassManager.h" // For the new PassManager.
+#include "llvm/Transforms/Utils/Mem2Reg.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Transforms/InstCombine/InstCombine.h"
+#include "llvm/Transforms/IPO/AlwaysInliner.h"
+#include "llvm/Passes/StandardInstrumentations.h"
 
 using namespace llvm;
 
-void runYourTurnPassPipelineForNewPM(Module &MyModule) {}
+void runYourTurnPassPipelineForNewPM(Module &MyModule) {
+    FunctionAnalysisManager FAM;
+    ModuleAnalysisManager MAM;
+
+    PassInstrumentationCallbacks PIC;
+    PrintPassOptions PrintPassOpts;
+    PrintPassOpts.Verbose = true;
+    PrintPassOpts.SkipAnalyses = false;
+    PrintPassOpts.Indent = true;
+
+    StandardInstrumentations SI(MyModule.getContext(), true, false, PrintPassOpts);
+    SI.registerCallbacks(PIC, &MAM);
+    
+    MAM.registerPass([&]{ return PassInstrumentationAnalysis(&PIC); });
+    FAM.registerPass([&] { return PassInstrumentationAnalysis(&PIC); });
+
+    MAM.registerPass([&] { return FunctionAnalysisManagerModuleProxy(FAM); });
+    FAM.registerPass([&] { return ModuleAnalysisManagerFunctionProxy(MAM); });
+    
+    PassBuilder PB;
+    PB.registerFunctionAnalyses(FAM);
+    PB.registerModuleAnalyses(MAM);
+
+    ModulePassManager MPM;
+
+    MPM.addPass(createModuleToFunctionPassAdaptor(
+          PromotePass()));
+    MPM.addPass(createModuleToFunctionPassAdaptor(
+          InstCombinePass()));
+
+    MPM.addPass(AlwaysInlinerPass());
+
+    MPM.run(MyModule, MAM);
+}
